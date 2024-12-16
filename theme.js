6f0c05e16005e904978483296757a92fced16ce3
ACai <63145205+KuiyueRO@users.noreply.github.com>
2024-12-16 17:27:16 +0800

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,13 +1,1 @@
-window.destroyTheme = () => {
-    const statusElement = document.getElementById("status");
-    if (statusElement) {
-        statusElement.style.backgroundColor = ""
-    }
-}
 
-(function() {
-    const statusElement = document.getElementById("status");
-    if (statusElement) {
-        statusElement.style.backgroundColor = "coral"
-    }
-})();
